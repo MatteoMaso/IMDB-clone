4d35c942e1b0550e1ef4a3b694e69dcb5d00e93a
matteo.maso <matteo.maso@advertima.com>
2023-10-15 23:41:55 +0200

    feat: create about page

---
+++ src/app/about/page.tsx
@@ -0,0 +1,30 @@
+export default function About() {
+    return (
+        // mx-auto: center the content when using the max width
+        <div className="max-w-6xl mx-4 sm:mx-auto space-y-4">
+            <h1 className="text-2xl font-medium text-amber-600">ABOUT</h1>
+            <p>
+                Founded in 2023, MovieDB is your go-to resource for movie, TV show, and celebrity information. Our platform offers a comprehensive database, catering to cinephiles, TV enthusiasts, and casual viewers alike.
+            </p>
+
+            <h2>Our Mission</h2>
+            <p>
+                At MovieDB, our mission is to connect the world's audiences with films and TV shows, making it easy for you to discover new content, track what you've watched, and engage with a global community of movie and TV enthusiasts.
+            </p>
+
+            <h2>What We Offer</h2>
+            <ul>
+                <li>Extensive movie and TV show listings with details like cast, crew, reviews, and ratings.</li>
+                <li>Latest entertainment news, including celebrity interviews, trailers, and exclusive content.</li>
+                <li>An interactive platform where users can create their watchlists, write reviews, and participate in discussions.</li>
+                <li>Up-to-date box office figures, tracking the success of current cinema releases.</li>
+                <li>A rich database of celebrity profiles, including filmographies, biographies, and trivia.</li>
+            </ul>
+
+            <h2>Join Our Community</h2>
+            <p>
+                Whether you're looking to find out more about that latest blockbuster, discover a hidden TV gem, or just chat about your favorite shows and films, MovieDB is the place for you. Join our ever-growing community and dive into the world of movies and television like never before!
+            </p>
+        </div>
+    )
+}
