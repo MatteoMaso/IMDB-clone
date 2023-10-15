01c98d9487cf05dfa558e5393e5596e2f6592a59
matteo.maso <matteo.maso@advertima.com>
2023-10-15 22:18:12 +0200

    feat: cleanup + add head component

---
+++ src/app/head.tsx
@@ -0,0 +1,10 @@
+export default function Head() {
+    return (
+        <>
+            <title>IMDB Clone</title>
+            <meta name="description" content="This is an IMDB clone to learn NextJS 13" />
+            <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+            <link rel="icon" href="/favicon.ico" />
+        </>
+    )
+}
