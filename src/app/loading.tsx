21e89f1c189c8a807149bfe28bad69a145b6f394
matteo.maso <matteo.maso@advertima.com>
2023-10-20 19:41:33 +0200

    feat: add loading effect

---
+++ src/app/loading.tsx
@@ -0,0 +1,7 @@
+export default function Loading() {
+    return (
+        <div className="flex justify-center mt-10">
+            <img className="h-96"  src="spinner.svg" alt="loading" />
+        </div>
+    )
+}
